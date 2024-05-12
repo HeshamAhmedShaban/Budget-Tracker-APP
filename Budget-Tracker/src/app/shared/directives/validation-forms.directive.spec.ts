28dd86c13384a970072de1123b3f91a1d5ff503a
Hesham Ahmed <heshamshaaban0098@gmail.com>
2024-05-12 09:13:53 +0300

    Create New App And file strucute

---
+++ Budget-Tracker/src/app/shared/directives/validation-forms.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ValidationFormsDirective } from './validation-forms.directive';
+
+describe('ValidationFormsDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ValidationFormsDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ Budget-Tracker/src/app/shared/directives/validation-forms.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appValidationForms]',
+  standalone: true
+})
+export class ValidationFormsDirective {
+
+  constructor() { }
+
+}
